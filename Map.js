59e0527547fd5ec16aff3f7d0d54afb6d198c4d2
Sam <amhajsaid@gmail.com>
2018-01-22 20:42:53 +0000

    100% working map with geolocation enabled !

--- Map.js
+++ Map.js
@@ -1,6 +1,6 @@
 
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import MapView from 'react-native-maps';
 
 class Map extends Component {
@@ -16,6 +16,12 @@ class Map extends Component {
   onPickLocation = event =>{
     
     const coords = event.nativeEvent.coordinate;
+    this.map.animateToRegion({
+      latitude : coords.latitude,
+      longitude : coords.longitude,
+      latitudeDelta: 0.04,
+      longitudeDelta: 0.09
+    })
     this.setState(
       {
         location: {
@@ -30,6 +36,28 @@ class Map extends Component {
         
     );
   }
+  getLocationHandler = () =>{
+    navigator.geolocation.getCurrentPosition(pos =>{
+      console.log(pos);
+
+      const coordsEvent = {
+        nativeEvent : {
+          coordinate : {
+            latitude : pos.coords.latitude,
+            longitude : pos.coords.longitude
+          }
+        }
+      };
+      this.onPickLocation(coordsEvent);
+
+    },
+    err => {
+      console.log(err);
+      alert('Fetching the position Failed, Please Pick one Manually!')
+    }
+  )
+  }
+
   render(){
      let marker  = null;
      if (this.state.locationChoosen){
@@ -37,22 +65,39 @@ class Map extends Component {
         />
      }
     return (
-      <MapView
-        initialRegion = {this.state.location}
-        style = {styles.mapStyles}
-        region  = { this.state.location}
-        onPress = { this.onPickLocation}
-      >
-         {marker}
-      </MapView>
+      <View style={{flex : 1}}>
+        <MapView
+          initialRegion = {this.state.location}
+          style = {styles.mapStyles}
+          onPress = { this.onPickLocation}
+          ref = {ref => this.map = ref}
+        >
+          {marker}
+        </MapView>
+        <View style= {styles.buttonStyles}>
+          <Button title ='Turn Geolocation On '
+          onPress = {this.getLocationHandler} 
+          color="rgba(255, 0, 0, 0.5)"
+          />
+        </View>
+      </View>
     )
   }
 }
 
 const styles = {
+  buttonStyles : {
+    borderRadius : 20,
+    alignSelf : "center",
+    position: 'absolute',
+    bottom: 20,
+    left: 10,
+    right: 10, 
+    
+  },
   mapStyles : {
     width : "100%",
-    height : "100%",
+    height : "90%",
     flex : 1,
   }
 };
